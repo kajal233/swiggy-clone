1128a58942a2038c2f7c58b3051bc8af5dc2d3b1
Kajal <89968244+kajal233@users.noreply.github.com>
2025-08-01 17:45:27 +0530

    new changes

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,10 +1,10 @@
 
-import { render } from "@testing-library/react";
+import { fireEvent, render ,screen } from "@testing-library/react";
  import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
 import Header from "../Header";
-
+import "@testing-library/jest-dom";
 
 test("should render header component", () => {
     render(
@@ -12,7 +12,21 @@ test("should render header component", () => {
             <Provider store={appStore}>
                 <Header />
             </Provider>
-
           </BrowserRouter>
     );
+    const logginButton = screen.getByRole("button");
+    expect(logginButton).toBeInTheDocument();
+});
+test("check login logout", () => {
+    render(
+       <BrowserRouter>
+            <Provider store={appStore}>
+                <Header />
+            </Provider>
+          </BrowserRouter>
+    );
+    const logginButton = screen.getByRole("button",{name:"🔐 Login"});
+    fireEvent.click(logginButton);
+    const logoutButton = screen.getByRole("button",{name:"🔓 Logout"});
+    expect(logoutButton).toBeInTheDocument();
 });
